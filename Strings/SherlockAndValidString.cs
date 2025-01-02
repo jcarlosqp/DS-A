5050d1a28f89d85b8c8af2c55921aa6256006030
Juan Carlos Quispe Pampa <jcarlos25@live.com>
2025-01-02 09:20:09 -0500

    Add files via upload

---
+++ Strings/SherlockAndValidString.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DSA.Strings.HackerRank
+{
+    public class SherlockAndValidString
+    {
+        //https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?isFullScreen=true
+        public static string isValid(string s)
+        {
+            var frec = new Dictionary<char, int>();
+            foreach (var c in s)
+                if (frec.ContainsKey(c))
+                    frec[c]++;
+                else
+                    frec.Add(c, 1);
+
+            var dist = frec.Values.Distinct().ToArray();
+            if (dist.Count() == 1)
+                return "YES";
+            else if (dist.Count() > 2)
+                return "NO";
+            else
+            {
+                int f0 = 0, f1 = 0;
+                foreach (int v in frec.Values)
+                    if (v == dist[0])
+                        f0++;
+                    else
+                        f1++;
+
+                return ((f0 == 1 || f1 == 1) && Math.Abs(dist[0] - dist[1]) == 1) ? "YES" : "NO";
+            }
+
+        }
+        public static void Test()
+        {
+            string value = "aabbccddeefghi";
+            Console.WriteLine($"value:{value}, result: {isValid(value)}");
+            
+        }
+    }
+}
---
+++ Strings/gameOfThronesI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DSA.Strings.HackerRank
+{
+    public class GameOfThronesI
+    {
+        //https://www.hackerrank.com/challenges/game-of-thrones/problem?isFullScreen=true
+        public static string gameOfThrones(string s)
+        {
+            var values = new Dictionary<char, int>();
+            foreach (var c in s)
+                if (values.ContainsKey(c))
+                    values[c]++;
+                else
+                    values.Add(c, 1);
+
+            var odds = values.Count(v => v.Value % 2 != 0);
+            return odds > 1 ? "NO" : "YES";
+        }
+        public static void Test()
+        {
+            string value = "aaabbbb";
+            Console.WriteLine($"value:{value}, result: {gameOfThrones(value)}");
+            
+        }
+    }
+}
